f4e3667effa759039007c16fec28eb373fa058eb
Mongezi Maluleka <mmaluleka023@student.wethinkcode.co.za>
2024-07-08 15:33:52 +0200

    ChatApp basic client and server

--- src/main/java/za/co/mongezi/javachatapp/Client/ChatClient.java
+++ src/main/java/za/co/mongezi/javachatapp/Client/ChatClient.java
@@ -1,12 +1,50 @@
 package za.co.mongezi.javachatapp.Client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 public class ChatClient {
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 2000);
-        System.out.println("connected to server");
+    private Socket socket = null;
+    private BufferedReader inputConsole = null;
+    private PrintWriter out = null;
+    private BufferedReader in = null;
+
+    public ChatClient(String address, int port) {
+        try {
+            //connect to chat server
+            socket = new Socket(address, port);
+            System.out.println("Connected to chat server");
+
+            //initialize input and output streams
+            inputConsole = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String line = "";
+            //read from the console and send to the server
+            while (!line.equals("exit")) {
+                line = inputConsole.readLine();
+                out.println(line);
+                System.out.println(in.readLine());
+            }
+            //close resources
+            socket.close();
+            inputConsole.close();
+            out.close();
+        } catch (UnknownHostException u) {
+            System.out.println("Host unkown" + u.getMessage());
+        } catch (IOException e) {
+            System.out.println("unexpected exception:" + e.getMessage());
+            throw new RuntimeException();
+        }
+    }
+
+    public static void main(String[] args) {
+        ChatClient client= new ChatClient("127.0.0.1",2000);
     }
 }
--- src/main/java/za/co/mongezi/javachatapp/Server/ChatServer.java
+++ src/main/java/za/co/mongezi/javachatapp/Server/ChatServer.java
@@ -1,17 +1,74 @@
 package za.co.mongezi.javachatapp.Server;
 
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatServer {
+
+    //List to keep track of all connected clients
+    private static List<ClientHandler> clients = new ArrayList<>();
+
+
     public static void  main(String[] args)throws IOException {
+        //create server socket listening on port 2000
         ServerSocket serverSocket = new ServerSocket(2000);
         System.out.println("Server started. waiting for clients..");
 
-        //accept client connections
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("client connected");
+        //continuously accept client connections
+        while(true) {
+            //accept client connections
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("client connected" + clientSocket);
+
+            //create new thread for each client
+            ClientHandler clientThread = new ClientHandler(clientSocket, clients);
+            clients.add(clientThread);
+            new Thread(clientThread).start();
+        }
+    }
+}
+class  ClientHandler implements Runnable{
+    private Socket clientSocket;
+    private List<ClientHandler> clients;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public ClientHandler(Socket socket, List<ClientHandler> clients) throws IOException {
+        this.clientSocket = socket;
+        this.clients = clients;
+        this.out = new PrintWriter(clientSocket.getOutputStream(),true);
+        this.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    @Override
+    public void run() {
+        try{
+            String inputLine;
+            //read mesage from client
+            while((inputLine = in.readLine()) != null) {
+                //broadcast  message to all clients
+                for (ClientHandler aClient:clients){
+                    aClient.out.println(inputLine);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred:" + e.getMessage());
+            throw new RuntimeException(e);
+        }finally {
+            try{
+                in.close();
+                out.close();
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
